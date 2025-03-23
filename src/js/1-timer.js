b0427f16abdcf487fe9b21d4ce673ca22e78d753
zeib01 <zeib01@users.noreply.github.com>
2025-03-23 21:36:20 +0100

    task 1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,3 +6,45 @@ import 'flatpickr/dist/flatpickr.min.css';
 const datetimePicker = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('#start-button');
 const timer = document.querySelector('.timer');
+
+let userSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+
+    if (Date.now() < selectedDates[0].getTime()) {
+      userSelectedDate = selectedDates[0];
+    } else {
+      window.alert('Please choose a date in the future');
+    }
+  },
+};
+flatpickr(datetimePicker, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
