f6f41b890a55ae0917c80aed4866f9b3a1a0c180
zeib01 <zeib01@users.noreply.github.com>
2025-03-23 20:39:57 +0100

    structure, html розмітка Task1 and Task 2

---
+++ src/js/1-timer.js
@@ -0,0 +1,8 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const datetimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('#start-button');
+const timer = document.querySelector('.timer');
